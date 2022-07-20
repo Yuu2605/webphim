32c9d5efe52de2c425589ffc56e2f5b2f77dc17b
Vũ Mạnh Tân <77037720+Yuu2605@users.noreply.github.com>
2022-07-20 13:51:40 +0700

    commit by yu

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\EpisodeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,25 +19,21 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [IndexController::class,'home'])->name('homepage');
-Route::get('/danh-muc', [IndexController::class,'category'])->name('category');
-Route::get('/the-loai', [IndexController::class,'genre'])->name('genre');
-Route::get('quoc-gia', [IndexController::class,'country'])->name('country');
-Route::get('/phim', [IndexController::class,'movie'])->name('movie');
-Route::get('/xem-phim', [IndexController::class,'watch'])->name('watch');
-Route::get('/episode', [IndexController::class,'episode'])->name('episode');
+Route::get('/', [IndexController::class, 'home'])->name('homepage');
+Route::get('/danh-muc/{slug}', [IndexController::class, 'category'])->name('category');
+Route::get('the-loai/{slug}', [IndexController::class, 'genre'])->name('genre');
+Route::get('/quoc-gia/{slug}', [IndexController::class, 'country'])->name('country');
+Route::get('/phim/{slug}', [IndexController::class, 'movie'])->name('movie');
+Route::get('/xem-phim', [IndexController::class, 'watch'])->name('watch');
+Route::get('/episode', [IndexController::class, 'episode'])->name('episode');
 
-Route::prefix('admin')->group(function () {
-    Auth::routes();
-
-});
+Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('category', CategoryController::class);
+Route::post('resorting', [CategoryController::class,'resorting'])->name('resorting');
+Route::resource('genre', GenreController::class);
+Route::resource('country', CountryController::class);
+Route::resource('episode', EpisodeController::class);
+Route::resource('movie', MovieController::class);
